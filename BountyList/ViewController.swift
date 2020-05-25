515c73a09a612859017d8e97f94def948f5ecbc7
cooo002 <cooo002@naver.com>
2020-05-25 21:36:29 +0900

    Initial Commit

---
+++ BountyList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by 김재석 on 2020/05/25.
+//  Copyright © 2020 김재석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
